7818c7a4b1969ef96deb798f8d08fe66384201aa
domin <domin@LAPTOP-MLSA3V7B>
2025-04-06 18:32:04 +0200

    jest

---
+++ Zadanie 8.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_8._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Napisać program wczytujący trzy długości odcinków i wypisujący na ekranie informację czy
+            //da się z tych odcinków zbudować trójkąt, czy też nie.
+            //Wskazówka: Z trzech odcinków można zbudować trójkąt, jeżeli suma długości dowolnych
+            //dwóch odcinków jest większa od długości trzeciego odcinka.
+
+            int a, b, c;
+
+            Console.Write("Podaj długość pierwszego odcinka: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj długość drugiego odcinka: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj długość trzeciego odcinka: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            if ((a + b > c) && (a + c > b) && (b + c > a))
+            {
+                Console.WriteLine("Można zbudować trójkąt");
+            }
+            else
+            {
+                Console.WriteLine("Nie można zbudować trójkąta");
+            }
+        }
+    }
+}
---
+++ Zadanie 9.1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_9._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+//            Dana jest funkcja kwadratowa f(x) = ax ^ 2 + bx + c.Napisać program wczytujący współczynniki
+//funkcji kwadratowej(a, b, c) i wypisujący na ekranie jej miejsca zerowe lub informację o ich
+//braku.
+//Wskazówka: Zbadaj deltę, wyznacz miejsca zerowe.
+
+            int a, b, c;
+            double del, x1, x2;
+
+            Console.Write("podaj a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("podaj b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("podaj c: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            del = b * b - 4 * a * c;
+
+            if (del > 0)
+            {
+                x1 = (-b + Math.Sqrt(del)) / (2 * a);
+                x2 = (-b - Math.Sqrt(del)) / (2 * a);
+                Console.WriteLine($"Miejsca zerowe to: {x1} i {x2}");
+            }
+            else if (del == 0)
+            {
+                x1 = -b / (2 * a);
+                Console.WriteLine($"Miejsce zerowe to: {x1}");
+            }
+            else
+            {
+                Console.WriteLine("Brak miejsc zerowych");
+            }
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
