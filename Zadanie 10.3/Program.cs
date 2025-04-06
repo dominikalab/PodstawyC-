f28cfa03661bed6212ad0a63f8405e6c64601d9e
domin <domin@LAPTOP-MLSA3V7B>
2025-04-06 21:08:53 +0200

    koniec

---
+++ Zadanie 10.3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_10._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //            Napisz program drukujący na ekranie kolejne liczby naturalne podzielne przez 7.Ilość liczb
+            //wczytaj z klawiatury. Użyj pętli for. Oto wydruk dla ile = 6:
+            //0, 7, 14, 21, 28, 35
+
+            int x;
+
+            Console.Write("Podaj ilość: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i<x; i++)
+            {
+                Console.Write($"{i*7}, ");
+            }
+            Console.ReadKey(true);
+        }
+    }
+}
